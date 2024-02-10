fa8e17f42b4822802acb90227ef17bea114d86d7
henriquecanella <henrique.mandelli@gmail.com>
2024-02-10 17:06:01 -0300

    feat: Add UserRepository

---
+++ backend/src/repositories/user.repository.ts
@@ -0,0 +1,5 @@
+import { UserEntity } from '../entities/UserEntities';
+
+export abstract class UserRepository {
+  abstract findMany(newSeed?: boolean, gender?: string, results: number): Promise<UserEntity[]>;
+}
